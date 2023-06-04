5ebb3624b1b0cc31c21490c0afe8219f90ec71ba
Naeim Mohammadi <naeim.mohammadi@bihe.org>
2023-06-04 12:20:55 +0000

    create Post and Tag models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+import os
+print(os.getenviron['CODIO_HOSTNAME'])
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.value
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
