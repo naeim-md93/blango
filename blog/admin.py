30e376889b91e6edc4fce86c7c4b9cd820f28be1
naeim_md93 <naeim.mohammadi@gmail.com>
2023-06-04 16:48:10 +0330

    *

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tag, Post
 
-# Register your models here.
+
+admin.site.register(Tag)
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    exclude = ['slug']
+    list_display = ['title', 'published_at']
+
+
+admin.site.register(Post, PostAdmin)
